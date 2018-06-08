6326ea8ffbca6e809d0e2d6fa4ffc9f4a53ca281
User <User@DESKTOP-4T7A8KU>
2018-06-08 08:28:26 +0300

    github

--- lines1/test/maincpp.cpp
+++ lines1/test/maincpp.cpp
@@ -4,27 +4,23 @@
 #include <ctime>
 #include <vector>
 #include <sstream>
-
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
 using namespace sf;
 using namespace std;
 
 
-
-
 struct Ball
 {
-
 	bool isFree = true;
 	Color color;
 	Sprite ballSprite;
-
 	
 } ball[9][9];
 
+
 int score=0;
 int countBalls=0;
-bool DeletedLines = true;
 
 void ThrowBalls(Texture &ballTexture)
 {
@@ -36,15 +32,13 @@ void ThrowBalls(Texture &ballTexture)
 	do
 	{
 		if (countBalls == 81) return;
-
 		int x = (rand() % 9);
 		int y = (rand()% 9);
 
 		if (ball[x][y].isFree)
-		{
-			
+		{		
 			ball[x][y].ballSprite.setTexture(ballTexture);
-			ball[x][y].color = color[rand() % 7];
+			ball[x][y].color = color[rand() %7];
 			ball[x][y].ballSprite.setColor(ball[x][y].color);
 			ball[x][y].ballSprite.setPosition(x*80, y*80+80);
 			
@@ -52,17 +46,70 @@ void ThrowBalls(Texture &ballTexture)
 			count++;
 			countBalls++;
 		}	
-	} while (count != 5);
+	} while (count != 3);
 
 }
 
 
-
-void checkLines()
+bool pathSearch(int startX, int startY, int endX, int endY)
 {
-	DeletedLines = true;
 	
-	//Проверка линий по горизонтали
+	int map[9][9];
+	for (int x = 0;x < 9;x++)
+	{
+		for (int y = 0;y < 9;y++)
+		{
+			if (ball[x][y].isFree) map[x][y] = -2;
+			else map[x][y] = -1;
+		}
+}
+	map[startX][startY] = 0;
+	map[endX][endY] = -2;
+   
+	int dx[4] = { 1, 0, -1, 0 };  
+	int dy[4] = { 0, 1, 0, -1 };   
+	
+	int step = 0;
+    int  x, y, k;
+    bool stop;
+	bool added;
+
+	do {
+		stop = true;
+		
+		for (y = 0; y < 9; ++y)
+			for (x = 0; x < 9; ++x)
+				if (map[x][y] == step)                         
+				{
+					for (k = 0; k < 4; ++k)                   
+					{
+						int iy = y + dy[k],
+							ix = x + dx[k];
+						if (iy >= 0 && iy < 9 && ix >= 0 && ix < 9 &&
+							map[ix][iy] == -2)
+						{
+							stop = false;
+							map[ix][iy] = step + 1;     
+
+						}
+					}		
+				}
+		
+		step++;
+
+	} while (!stop&& map[endX][endY] == -2);
+
+	if (map[endX][endY]==-2) return false;
+	else return true;
+}
+
+
+
+
+bool checkLines1()	//Проверка линий по горизонтали
+
+{
+	bool DeletedLines = true;
 
 	for (int y = 0;y < 9;y++)
 	{
@@ -81,14 +128,17 @@ void checkLines()
 			{
 				if (count >= 5)
 				{
-					for (int i = left;i <x;i++)ball[i][y].isFree = true;
+					for (int i = left;i < x;i++)ball[i][y].isFree = true;
 					DeletedLines = false;
 					score += count;
 					countBalls -= count;
 				}
 
 				if (ball[x][y].isFree)
+				{
 					count = 0;
+					color = Color::Black;
+				}
 
 				else
 				{
@@ -97,10 +147,9 @@ void checkLines()
 					color = ball[x][y].color;
 				}
 			}
-
 		}
 
-		if (count >= 3)
+		if (count >= 5)
 		{
 			for (int i = left;i <= 8;i++)ball[i][y].isFree = true;
 			DeletedLines = false;
@@ -110,24 +159,28 @@ void checkLines()
 
 	}
 
-	//Проверка линий по вертикали
+	return DeletedLines;
+}
 
+
+
+bool checkLines2()//Проверка линий по вертикали
+
+{
+	bool DeletedLines = true;
+	
 	for (int x = 0;x < 9;x++)
 	{
 		Color color = Color::Black;
-
 		int count = 0;
 		int top = 0;
 
-
 		for (int y = 0;y < 9;y++)
 		{
-
 			if ((color == ball[x][y].color) && (!ball[x][y].isFree))
 				count++;
 
-
-			if ((color != ball[x][y].color) && (!ball[x][y].isFree) || (ball[x][y].isFree))
+			if ((color != ball[x][y].color) && !ball[x][y].isFree || (ball[x][y].isFree))
 			{
 				if (count >= 5)
 				{
@@ -138,8 +191,10 @@ void checkLines()
 				}
 
 				if (ball[x][y].isFree)
+				{
 					count = 0;
-
+					color = Color::Black;
+				}
 
 				else
 				{
@@ -147,9 +202,7 @@ void checkLines()
 					count = 1;
 					color = ball[x][y].color;
 				}
-
 			}
-
 		}
 		if (count >= 5)
 		{
@@ -160,14 +213,270 @@ void checkLines()
 		}
 	}
 
-
+	return DeletedLines;
 
 }
 
 
+bool checkLines3()//Проверка линий по диагонали слева направо над главной диагональю
+{
+	bool DeletedLines = true;
+
+	for (int x = 0;x <= 5;x++)
+	{
+		int i = x;
+		int k = 0;
+		int count = 0;
+		Color color = Color::Black;
+		int topI = x;
+		int topK = 0;
+
+		do {
+			if (!ball[i][k].isFree && (ball[i][k].color == color))
+				count++;
+
+			if ((!ball[i][k].isFree && (ball[i][k].color != color)) || (ball[i][k].isFree))
+			{
+				if (count >= 5)
+				{
+					for (;((topI < i) && (topK < k));((topI++) && (topK++))) ball[topI][topK].isFree = true;
+					DeletedLines = false;
+					score += count;
+					countBalls -= count;
+				}
+				if (ball[i][k].isFree)
+				{
+					count = 0;
+					color = Color::Black;
+				}
+
+				else
+				{
+					color = ball[i][k].color;
+					count = 1;
+					topI = i;
+					topK = k;
+				}
+			}
+			i++;
+			k++;
+		} while ((i < 9) && (k < 9));
+
+		if (count >= 5)
+		{
+			for (;((topI < 9) && (topK < 9));((topI++) && (topK++))) ball[topI][topK].isFree = true;
+			DeletedLines = false;
+			score += count;
+			countBalls -= count;
+		}
+	}
+
+	return DeletedLines;
+}
 
 
+bool checkLines4()
+{
+	bool DeletedLines = true;
 
+    for (int y = 0;y <= 5;y++)
+	{
+		int i = 0;
+		int k = y;
+		int count = 0;
+		Color color = Color::Black;
+		int topI = 0;
+		int topK = y;
+
+		do {
+			if (!ball[i][k].isFree && (ball[i][k].color == color))
+				count++;
+
+			if ((!ball[i][k].isFree && (ball[i][k].color != color)) || (ball[i][k].isFree))
+			{
+				if (count >= 5)
+				{
+					do {
+					    ball[topI][topK].isFree = true;
+						DeletedLines = false;
+						
+						topI++;
+						topK++;
+					} while ((topI < i) && (topK < k));
+					
+					score += count;
+					countBalls -= count;
+				}
+				if (ball[i][k].isFree)
+				{
+					count = 0;
+					color = Color::Black;
+				}
+
+				else
+				{
+					color = ball[i][k].color;
+					count = 1;
+					topI = i;
+					topK = k;
+				}
+			}
+			i++;
+			k++;
+		} while ((i < 9) && (k < 9));
+
+		if (count >= 5)
+		{
+			do {
+				ball[topI][topK].isFree = true;
+				DeletedLines = false;
+				topI++;
+				topK++;
+			} while ((topI < i) && (topK < k));
+			score += count;
+			countBalls -= count;
+		}
+	}
+	return DeletedLines;
+}
+
+
+bool checkLines5()//проверка диагоналей справо налево
+{
+	bool DeletedLines = true;
+
+	for (int x = 4;x <9;x++)
+	{
+		int i = x;
+		int k = 0;
+		int count = 0;
+		Color color = Color::Black;
+		int topI = x;
+		int topK = 0;
+
+		do {
+			if ((!ball[i][k].isFree) && (ball[i][k].color == color))
+				count++;
+
+			if ((!ball[i][k].isFree && (ball[i][k].color != color)) || (ball[i][k].isFree))
+			{
+				if (count >= 5)
+				{
+					do {
+						ball[topI][topK].isFree = true;
+						topI--;
+						topK++;
+					} while (topI >= i && topK < k);
+					DeletedLines = false;
+					score += count;
+					countBalls -= count;
+				}
+				if (ball[i][k].isFree)
+				{
+					count = 0;
+					color = Color::Black;
+				}
+
+				else
+				{
+					color = ball[i][k].color;
+					count = 1;
+					topI = i;
+					topK = k;
+				}
+			}
+			i--;
+			k++;
+		} while ((i >=0) && (k < 9));
+
+		if (count >= 5)
+		{
+			do {
+				 ball[topI][topK].isFree = true; 
+				topI--;
+				topK++;
+			} while (topI >= 0 && topK < 9);
+			DeletedLines = false;
+			score += count;
+			countBalls -= count;
+		}
+	}
+
+	return DeletedLines;
+}
+
+bool checkLines6() 
+{
+	bool DeletedLines = true;
+
+	for (int y = 1;y <5;y++)
+	{
+		int i = 8;
+		int k = y;
+		int count = 0;
+		Color color = Color::Black;
+		int topI = 8;
+		int topK = y;
+
+		do {
+			if ((!ball[i][k].isFree) && (ball[i][k].color == color))
+				count++;
+
+			if ((!ball[i][k].isFree && (ball[i][k].color != color)) || (ball[i][k].isFree))
+			{
+				if (count >= 5)
+				{
+					do {
+						ball[topI][topK].isFree = true;
+						topI--;
+						topK++;
+						count--;
+					} while (count>0);
+					DeletedLines = false;
+					score += count;
+					countBalls -= count;
+				}
+				if (ball[i][k].isFree)
+				{
+					count = 0;
+					color = Color::Black;
+				}
+
+				else
+				{
+					color = ball[i][k].color;
+					count = 1;
+					topI = i;
+					topK = k;
+				}
+			}
+			i--;
+			k++;
+		} while ((i >= 0) && (k < 9));
+
+		if (count >= 5)
+		{
+			do {
+				ball[topI][topK].isFree = true;
+				topI--;
+				topK++;
+				count--;
+			} while (count>0);
+			DeletedLines = false;
+			score += count;
+			countBalls -= count;
+		}
+	}
+	return DeletedLines;
+
+}
+
+
+bool checkLines()
+{
+	if (checkLines1() && checkLines2() && checkLines3() && checkLines4()&&checkLines5()&&checkLines6()) return true;
+	else return false;
+}
 
 int main()
 {
@@ -175,59 +484,63 @@ int main()
 	
 	RenderWindow window(VideoMode(720, 800), "lines");	
 	
-
 	Texture fieldTexture;
 	fieldTexture.loadFromFile("field2.png");
 	
 	Sprite fieldSprite;
 	fieldSprite.setTexture(fieldTexture);
 	
-
 	Image ballImage;
 	ballImage.loadFromFile("ball.png");
 
 	Texture ballTexture;
 	ballTexture.loadFromImage(ballImage);
 		
-
-
 	Font font;//шрифт 
 	font.loadFromFile("font.ttf");//передаем нашему шрифту файл шрифта
 	Text text("", font, 40);//создаем объект текст. закидываем в объект текст строку, шрифт, размер шрифта(в пикселях);//сам объект текст (не строка)
 	text.setFillColor(Color::Blue);//покрасили текст в красный. если убрать эту строку, то по умолчанию он белый
 	
 
-
 	int x, y;
 
 	bool isMove = false;
 	bool isSelect = false;
-	bool Throw = false;
+	bool Throw =true;
 	
-	
-	ThrowBalls(ballTexture);
+
+	int FPS = 80; 
+	int msSleepTime = 1000 / FPS;
+	sf::Clock sleepClock;
+
 	
 	while (window.isOpen())
 	{
+	
+		sf::Time sleepTime = sf::milliseconds(msSleepTime - sleepClock.getElapsedTime().asMilliseconds());
+		if (sleepTime.asMilliseconds() > 0) sf::sleep(sleepTime);
+		
+		sleepClock.restart();
+
 		Event event;
 
-		Vector2i pixelPos = Mouse::getPosition(window);//забираем коорд курсора
-		Vector2f pos = window.mapPixelToCoords(pixelPos);//переводим их в игровые (уходим от коорд окна)
-
+		Vector2i pixelPos = Mouse::getPosition(window);
+		Vector2f pos = window.mapPixelToCoords(pixelPos);
 		
-
+		if (Throw && checkLines())
+				ThrowBalls(ballTexture);
+	
+		Throw = false;
+	
 		while (window.pollEvent(event));
 		{
-
 
 			if (event.type == Event::Closed)
 				window.close();
 
-
-			
+	
 				if ((event.type == Event::MouseButtonPressed) && (event.key.code == Mouse::Left))
 				{
-
 					x = pos.x / 80;
 					y = pos.y / 80 - 1;
 
@@ -243,14 +556,13 @@ int main()
 					selectedX = (int)(pos.x / 80);
 					selectedY = (int)(pos.y / 80 - 1);
 
-
 					isSelect = false;
-					isMove = true;
+					if (ball[selectedX][selectedY].isFree)isMove = true;
 				}
 
-				if (isMove)
+				if(isMove)
 				{
-					if (ball[selectedX][selectedY].isFree)
+					if (pathSearch(x, y, selectedX, selectedY)==true)
 					{
 						ball[selectedX][selectedY].ballSprite.setTexture(ballTexture);
 						ball[selectedX][selectedY].color = ball[x][y].color;
@@ -259,30 +571,16 @@ int main()
 
 						ball[selectedX][selectedY].isFree = false;
 						ball[x][y].isFree = true;
-						isMove = false;
 						Throw = true;
-						
 					}
+				 	    isMove = false;
 				}
 			}
-
-
-	     checkLines();
-
-			if ((Throw) &&(DeletedLines))
 				
-			{
-				ThrowBalls(ballTexture);
-				Throw = false;			
-			}
-		
-
-		
+			
 		{
 			window.clear(Color(255, 255, 255));
 			window.draw(fieldSprite);
-
-
 
 		    ostringstream scoreString;    // объявили переменную
 			scoreString << score;		//занесли в нее число очков, то есть формируем строку
@@ -294,24 +592,23 @@ int main()
 			for (int i = 0;i < 9;i++)
 			{
 				for (int k = 0;k < 9;k++)
-
 					if (!ball[i][k].isFree)  window.draw(ball[i][k].ballSprite);
 			}
 
-          if(countBalls==81)
-            {
-				Text GameOver("", font, 80);
-				GameOver.setFillColor(Color::Black);
-				GameOver.setString("Game Over");
-				GameOver.setPosition(100, 360);
-				window.draw(GameOver);
-			}
 
-			window.display();
-	
+			if (countBalls >= 81)
+	{
+		Text GameOver("", font, 80);
+		GameOver.setFillColor(Color::Black);
+		GameOver.setString("Game Over");
+		GameOver.setPosition(100, 360);
+		window.draw(GameOver);
+	}
+    
+			window.display();		
 		}
-		
 	}
 
+	
 	return 0;
 }
